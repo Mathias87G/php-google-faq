2ca01a8f2335cf184cd0a99a5f7d71e3a77de8be
Mathias <mathias.grandin@gmail.com>
2020-09-17 16:01:17 +0200

    aggiunto foreach per scelta lingue e commenti

--- index.php
+++ index.php
@@ -25,6 +25,8 @@ $FAQS = [
   'Risposta' => 'In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l\'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l\'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell\'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell\'URL referrer potrebbero essere disponibili mediante Google Analytics o un\'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all\'esatte parole chiave che hanno determinato il clic su un annuncio.'
   ]
 ];
+
+$languages = ['Italiano', 'English', 'Deutsch', 'Francais', 'Afrikaans'];
  ?>
 
 <!-- html -->
@@ -78,11 +80,11 @@ $FAQS = [
           </ul>
           <div class="lang-select">
             <i class="fas fa-globe-europe"></i>
+            <!-- foreach per lingue -->
             <select id="language" name="language">
-              <option value="it">Italiano</option>
-              <option value="fr">Francais</option>
-              <option value="de">Deutsch</option>
-              <option value="en">English</option>
+              <?php foreach($languages as $language) {?>
+                <option value="<?php echo $language?>"><?php echo $language?></option>
+              <?php } ?>
             </select>
           </div>
         </div>
